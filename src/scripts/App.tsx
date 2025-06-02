ec22222fbec52863a852a7821b0fc5f6903dd5ef
peter 0_0 <p.larionov.45@seznam.cz>
2025-06-02 10:01:16 +0200

    Add files via upload

---
+++ src/scripts/App.tsx
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import Avatar from './Avatar.tsx'; 
+
+function App() {
+  return (
+    <div className='App'>
+      <Avatar />
+    </div>
+  )
+}
+
+export default App
